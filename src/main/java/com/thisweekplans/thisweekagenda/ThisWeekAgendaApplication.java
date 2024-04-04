a2917a9039677e27fd659cd90c59a337ad8865cf
cleverclope <cleverarinda@gmail.com>
2024-04-04 11:23:20 +0300

    Initial commit

---
+++ src/main/java/com/thisweekplans/thisweekagenda/ThisWeekAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.thisweekplans.thisweekagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisWeekAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisWeekAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thisweekplans/thisweekagenda/ThisWeekAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thisweekplans.thisweekagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisWeekAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
